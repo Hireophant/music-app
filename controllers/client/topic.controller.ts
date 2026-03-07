8de9f2fb719dde0269a7d6e3d018d89e2bed807f
Hireophant <tntdanh2506@gmail.com>
2026-03-07 16:22:46 +0700

    strcut pug

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -6,6 +6,7 @@ import Topic from "../../models/topic.model";
 export const topics = async (req: Request, res: Response) => {
     const topics = await Topic.find({ deleted: false });
     res.render("client/pages/topics/index", {
+        pageTitle: "Chủ đề bài hát",
         topics: topics
     });
 }
