8ada90166ba67960c1cbf1bdcbcc1aa61995c773
Hireophant <tntdanh2506@gmail.com>
2026-03-07 15:46:16 +0700

    connect database

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
-console.log("Hello World");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+import Topic from "./models/topic.model";
+
+dotenv.config();
+
+database.connect();
+
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+
+
+
+app.get("/topics", async (req: Request, res: Response) => {
+    const topics = await Topic.find({ deleted: false });
+    res.render("client/pages/topics/index", {
+        topics: topics
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
