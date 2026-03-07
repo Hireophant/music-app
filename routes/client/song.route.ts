5524cfcc5b5a283b6eef596a9e62d4d56f299fac
Hireophant <tntdanh2506@gmail.com>
2026-03-08 06:37:42 +0700

    song favor button

--- routes/client/song.route.ts
+++ routes/client/song.route.ts
@@ -5,5 +5,7 @@ const router: Router = Router();
 router.get("/:slugTopic", controller.list);
 router.get("/detail/:slugSong", controller.detail);
 router.patch("/like/:typeLike/:idSong", controller.like);
+router.patch("/favorite/:typeFavorite/:idSong", controller.favorite);
+
 
 export default router;
