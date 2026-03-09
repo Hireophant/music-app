a62f022cc81f13a2d3fb668862df080723ec9b6e
Hireophant <tntdanh2506@gmail.com>
2026-03-10 04:11:11 +0700

    layout admin

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
 import clientRoutes from "./routes/client/index.route";
+import adminRoutes from "./routes/admin/index.route";
+
+import { systemConfig } from "./config/config";
+import path from "path";
 
 dotenv.config();
 
@@ -19,6 +23,20 @@ app.set("view engine", "pug");
 
 // Client routes
 clientRoutes(app);
+
+// Admin routes
+adminRoutes(app);
+
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce")),
+);
+// End TinyMCE
+
+
+// App local variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
