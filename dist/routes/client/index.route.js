2cf2137cd02f6bf8f6aa105091fcb27e0331dafc
Hireophant <tntdanh2506@gmail.com>
2026-03-11 05:09:55 +0700

    create folder dist

---
+++ dist/routes/client/index.route.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const topic_route_1 = __importDefault(require("./topic.route"));
+const song_route_1 = __importDefault(require("./song.route"));
+const favorite_song_route_1 = __importDefault(require("./favorite-song.route"));
+const search_route_1 = __importDefault(require("./search.route"));
+const clientRoutes = (app) => {
+    app.use("/topics", topic_route_1.default);
+    app.use("/songs", song_route_1.default);
+    app.use("/favorite-songs", favorite_song_route_1.default);
+    app.use("/search", search_route_1.default);
+};
+exports.default = clientRoutes;
--- index.ts
+++ index.ts
@@ -18,12 +18,12 @@ const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 app.use(methodOverride("_method"));
 
 
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 // Client routes
